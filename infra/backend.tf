8f45814ecf77654a5dfdfd264ed081f831f4a454
Maitra Patel <10maitra22@gmail.com>
2026-03-19 17:08:58 -0600

    stop tracking backend.tf

--- infra/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tfstate-us-west-2-43262389"
-    key            = "infra/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "tflock-us-west-2-43262389"
-    encrypt        = true
-  }
-}
